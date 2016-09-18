608f16a7b4134a98fd644e8d78298abe7b21e81e
Timo Furrer <tuxtimo@gmail.com>
2016-09-18 16:48:23 +0200

    Add author to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='click-man',
     version='0.1.0',
     license='MIT',
+    author='Timo Furrer',
+    author_email='tuxtimo@gmail.com',
     install_requires=[
         'click'
     ],
