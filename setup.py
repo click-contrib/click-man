ba05d89c87e7f20452c9bbacae171f4be3b42775
Timo Furrer <tuxtimo@gmail.com>
2016-09-27 20:30:43 +0200

    Improve releasing

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='click-man',
     version='0.1.0',
     license='MIT',
+    description='Generate man pages for click based CLI applications',
     author='Timo Furrer',
     author_email='tuxtimo@gmail.com',
     install_requires=[
