dd17df4860b02d6412d2227929191d5f47519d9c
Stephen Rosen <sirosen@globus.org>
2020-08-31 15:30:54 +0000

    Support commands with hidden options
    When an option has hidden=True set, Option.get_help_record returns
    None. Simply filter these out to avoid a later failure when trying to
    unpack helptext.

--- click_man/core.py
+++ click_man/core.py
@@ -32,7 +32,12 @@ def generate_man_page(ctx, version=None):
     man_page.short_help = ctx.command.get_short_help_str()
     man_page.description = ctx.command.help
     man_page.synopsis = ' '.join(ctx.command.collect_usage_pieces(ctx))
-    man_page.options = [x.get_help_record(ctx) for x in ctx.command.params if isinstance(x, click.Option)]
+    man_page.options = [
+        y for y in
+        [x.get_help_record(ctx) for x in ctx.command.params
+         if isinstance(x, click.Option)]
+        if y
+    ]
     commands = getattr(ctx.command, 'commands', None)
     if commands:
         man_page.commands = [
